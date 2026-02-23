5cfa2fdccb6fde8526e858f9cceff9eb42de9194
Notos <lianggh159@outlook.com>
2026-02-23 16:54:10 +0800

    develop and use ToggleThemeFooterBtn component

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
   <Header />
   <RouterView />
   <!-- <Footer /> -->
-  <ToggleTheme />
+  <ToggleThemeFooterBtn />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 // import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
-import ToggleTheme from '@/components/ToggleTheme.vue'
+import ToggleThemeFooterBtn from '@/components/ToggleThemeFooterBtn.vue'
 </script>
 
 <style scoped>
---
+++ src/components/ToggleThemeFooterBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <ToggleTheme class="footer-btn theme-toggler">
+    <i class="svg-icon" :class="themeClass" />
+  </ToggleTheme>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useIsDarkStore } from '@/stores'
+import ToggleTheme from './ToggleTheme.vue'
+
+const isDarkStore = useIsDarkStore()
+const themeClass = computed(() => {
+  return isDarkStore.isDark ? 'icon-theme-dark' : 'icon-theme-light'
+})
+</script>
