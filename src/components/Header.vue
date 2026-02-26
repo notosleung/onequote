2e3376b1616ae268e8355727af8608994627b0b0
Notos <lianggh159@outlook.com>
2026-02-26 17:48:54 +0800

    update Header: add toggleDark

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,15 +2,21 @@
   <div class="header">
     <a href="javascript:void(0)" class="logo" />
     <nav class="menu">
+      <router-link to="/" class="hover-underline">
+        首页
+      </router-link>
       <router-link to="/about" class="hover-underline">
         关于
       </router-link>
+      <a href="javascript:void(0)" class="hover-underline" @click="toggleDark">
+        {{ isDark ? '开灯' : '关灯' }}
+      </a>
     </nav>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { isDark, toggleDark } from '@/stores'
 </script>
 
 <style scoped>
