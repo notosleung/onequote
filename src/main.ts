8fa06eb9b5d1573f3b5030eae1fbd04d690c893a
Notos <lianggh159@outlook.com>
2026-02-26 16:11:44 +0800

    use ssg config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
-import router from './router'
 import './assets/styles/common.css'
 import './assets/styles/theme-dark.css'
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+export const createApp = ViteSSG(
+  App,
+  {
+    routes,
+    base: import.meta.env.BASE_URL,
+  },
+  ({ app, initialState }) => {
+    const pinia = createPinia()
+    app.use(pinia)
+
+    // 客户端激活时恢复状态
+    if (!import.meta.env.SSR) {
+      pinia.state.value = initialState.pinia || {}
+    }
+    else {
+      initialState.pinia = pinia.state.value
+    }
+  },
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,36 @@
 import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import Markdown from 'unplugin-vue-markdown/vite'
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue({
-    include: [/\.vue$/, /\.md$/],
-  }), Markdown({
-    markdownItOptions: {
-      html: true,
-      linkify: true,
-      typographer: true,
-    },
-  })],
+  plugins: [
+    VueRouter({
+      extensions: ['.vue', '.md'],
+      dts: 'src/typed-router.d.ts',
+    }),
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown({
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
+  },
+  // SSG 配置
+  ssgOptions: {
+    script: 'async',
+    formatting: 'minify',
   },
 })
