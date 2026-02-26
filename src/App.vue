1fbe96045fcc0481bf0c9403ce0da5b6d6719e90
Notos <lianggh159@outlook.com>
2026-02-26 17:44:52 +0800

    remove ToggleThemeFooterBtn

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,12 @@
     </main>
     <Footer />
   </div>
-  <ToggleThemeFooterBtn />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Footer from '@/components/Footer.vue'
 import Header from '@/components/Header.vue'
-import ToggleThemeFooterBtn from '@/components/ToggleThemeFooterBtn.vue'
 </script>
 
 <style scoped>
