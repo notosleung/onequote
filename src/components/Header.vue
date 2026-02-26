2b0de36ddc2beb779c86f3e755b3879fddbd3d73
Notos <lianggh159@outlook.com>
2026-02-26 21:46:24 +0800

    logo become a link to home, change word of switching off light

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <a href="javascript:void(0)" class="logo" />
+    <router-link to="/" class="logo" />
     <nav class="menu">
       <router-link to="/" class="hover-underline">
         首页
@@ -9,7 +9,7 @@
         关于
       </router-link>
       <a href="javascript:void(0)" class="hover-underline" @click="toggleDark">
-        {{ isDark ? '开灯' : '关灯' }}
+        {{ isDark ? '开灯' : '熄灯' }}
       </a>
     </nav>
   </div>
