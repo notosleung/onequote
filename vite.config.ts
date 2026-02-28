2d5d71e69404cee1e7ee06734929d350ffcf7820
Notos <lianggh159@outlook.com>
2026-03-01 06:15:47 +0800

    fix: change base from relative to absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: './',
+  base: '/',
   plugins: [
     VueRouter({
       extensions: ['.vue', '.md'],
